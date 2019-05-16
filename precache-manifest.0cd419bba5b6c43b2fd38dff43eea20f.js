72d277c1ef2b446495815a7d6075cb3061c99441
EM <aslam4webz@gmail.com>
2019-05-17 00:51:46 +0530

    Updates

---
+++ precache-manifest.0cd419bba5b6c43b2fd38dff43eea20f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69cda014d953a428e6e9ed4fdf7c7915",
+    "url": "/React-Food-Storm/index.html"
+  },
+  {
+    "revision": "a5552bf13b64f2a38c29",
+    "url": "/React-Food-Storm/static/css/main.4ed9da7f.chunk.css"
+  },
+  {
+    "revision": "136fa172f76dfd716d2f",
+    "url": "/React-Food-Storm/static/js/2.4ce393e1.chunk.js"
+  },
+  {
+    "revision": "a5552bf13b64f2a38c29",
+    "url": "/React-Food-Storm/static/js/main.5f98587d.chunk.js"
+  },
+  {
+    "revision": "945acb248526905b83df",
+    "url": "/React-Food-Storm/static/js/runtime~main.7ddbab1d.js"
+  }
+]);
